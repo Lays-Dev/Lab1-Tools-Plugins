6a3e98830ce9c84cce21d639e8d14fdc38271275
Metroidman97 <chrismetroidman97@gmail.com>
2026-09-04 21:13:16 -0400

    Added possible moves when selecting a piece

--- Lab1 Chess/Assets/Scripts/pieces.cs
+++ Lab1 Chess/Assets/Scripts/pieces.cs
@@ -17,12 +17,15 @@ public class pieces : MonoBehaviour
         King
     }
 
-    public PieceType pieceType;         // Public enum variable for selecting piece type
+    public PieceType pieceType;                 // Public enum variable for selecting piece type
 
-    public Color tint = Color.white;    // Public color picker for changing the piece color in the editor
+    public Color tint = Color.white;            // Public color picker for changing the piece color in the editor
+
+    public Color outlineColor = Color.yellow;   // Customize the outline color in the inspector
+
+    public Color pathColor = Color.white;       // Public color picker for the piece path markers
 
     // Array of texture names for different chess pieces.
-
     public string[] pieceIcons =
     {
         "Chess_plt60",
@@ -33,17 +36,13 @@ public class pieces : MonoBehaviour
         "Chess_klt60",
     };
 
-    // This method fills in data from the piece type enum and the icon array to draw the selected texture
 
+
+    // This method fills in data from the piece type enum and the icon array to draw the selected texture
     void OnDrawGizmos()
     {
         Gizmos.DrawIcon(transform.position, pieceIcons[(int)pieceType], true, tint);
     }
-
-
-// Customize the outline color in the inspector
-
-    public Color outlineColor = Color.yellow;
 
 // Used to run handles in the editor window
 #if UNITY_EDITOR
@@ -55,29 +54,158 @@ public class pieces : MonoBehaviour
         // Specific float values because the "board" covers the outline
         Handles.DrawWireCube(transform.position, new Vector3(.9f, .9f, 0f));
 
+        // Show the different valid moves when the piece is selected
+        ShowMoves();
     }
 
 #endif
 
 
     
-    /*
-    void OnDrawGizmos()
+    
+    void ShowMoves()
     {
-        // Change piece type based on the enum
+        Gizmos.color = pathColor;   // Change the color of the piece paths
+
+        // Change visible moves based on the piece type
         switch(pieceType)
         {
 
             case(PieceType.Pawn):
-            Gizmos.DrawIcon(transform.position,"Chess_plt60", true, tint);
+                // Because pawns only have one possible move, they don't need a dedicated separate function
+                Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y + 2f, 0f));
             break;
 
             case(PieceType.Knight):
-            Gizmos.DrawIcon(transform.position,"Chess_nlt60", true, tint);
+                DrawKnightPaths();
+            break;
+
+            case (PieceType.Rook):
+                DrawRookPaths();
+            break;
+
+            case (PieceType.Bishop):
+                DrawBishopPaths();
             break;
             
+            case (PieceType.Queen):
+                DrawQueenPaths();
+            break;
+
+            case (PieceType.King):
+                DrawKingPaths();
+            break;
         }
         
     }
-    */
+
+    void DrawKnightPaths()
+    {
+        Vector3[] kightPaths =
+        {
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 1f, transform.position.y + 2f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 2f, transform.position.y + 1f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 1f, transform.position.y - 2f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 2f, transform.position.y - 1f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 1f, transform.position.y + 2f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 2f, transform.position.y + 1f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 1f, transform.position.y - 2f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 2f, transform.position.y - 1f, 0f)
+        };
+
+        Gizmos.DrawLineList(kightPaths);
+    }
+
+    void DrawRookPaths()
+    {
+        Vector3[] rookPaths =
+        {
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y + 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 7f, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y - 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 7f, transform.position.y, 0f)
+        };
+
+        Gizmos.DrawLineList(rookPaths);
+    }
+
+    void DrawBishopPaths()
+    {
+        Vector3[] bishopPaths =
+        {
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 7f, transform.position.y + 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 7f, transform.position.y - 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 7f, transform.position.y + 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 7f, transform.position.y - 7f, 0f)
+        };
+
+        Gizmos.DrawLineList(bishopPaths);
+    }
+
+    void DrawQueenPaths()
+    {
+        Vector3[] queenPaths =
+        {
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y + 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 7f, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y - 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 7f, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 7f, transform.position.y + 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 7f, transform.position.y - 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 7f, transform.position.y + 7f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 7f, transform.position.y - 7f, 0f)
+        };
+
+        Gizmos.DrawLineList(queenPaths);
+    }
+
+    void DrawKingPaths()
+    {
+        Vector3[] kingPaths =
+        {
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y + 1f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 1f, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y - 1f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 1f, transform.position.y, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 1f, transform.position.y + 1f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x + 1f, transform.position.y - 1f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 1f, transform.position.y + 1f, 0f),
+            new Vector3(transform.position.x, transform.position.y, 0f),
+            new Vector3(transform.position.x - 1f, transform.position.y - 1f, 0f)
+        };
+
+        Gizmos.DrawLineList(kingPaths);
+    }
+    
 }
